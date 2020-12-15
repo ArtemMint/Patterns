bacdabbf1b4904292f586d53ec11145036af0c46
ArtemMint <nemnovus@gmail.com>
2020-12-15 22:03:11 +0200

    add decorator

---
+++ Structural/decorator.py
@@ -0,0 +1,26 @@
+class Human():
+    """Class human"""
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print(f'Hello! My name is {self._name}')
+
+
+class Plan:
+    """Class Plan"""
+    def __init__(self, human):
+        self._human = human
+
+    def __getattr__(self, item):
+        return getattr(self._human, item)
+
+    def fly(self):
+        print(f'{self._human._name} fly on the plan.')
+
+
+man = Human('James')
+
+plan = Plan(man)
+plan.say()
+plan.fly()
