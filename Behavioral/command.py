0b78f40696f23af8c5ba159b996cd508c29adeb9
ArtemMint <nemnovus@gmail.com>
2020-12-16 14:31:33 +0200

    add command.py

---
+++ Behavioral/command.py
@@ -0,0 +1,44 @@
+class Light(object):
+    def turn_on(self):
+        print('Turn on light')
+
+    def turn_off(self):
+        print('Turn off light')
+
+
+class CommandBase(object):
+    def execute(self):
+        raise NotImplementedError()
+
+
+class LightCommandBase(CommandBase):
+    def __init__(self, light):
+        self.light = light
+
+
+class TurnOnLightCommand(LightCommandBase):
+    def execute(self):
+        self.light.turn_on()
+
+
+class TurnOffLightCommand(LightCommandBase):
+    def execute(self):
+        self.light.turn_off()
+
+
+class Switch(object):
+    def __init__(self, on_cmd, off_cmd):
+        self.on_cmd = on_cmd
+        self.off_cmd = off_cmd
+
+    def on(self):
+        self.on_cmd.execute()
+
+    def off(self):
+        self.off_cmd.execute()
+
+
+light = Light()
+switch = Switch(on_cmd=TurnOnLightCommand(light), off_cmd=TurnOffLightCommand(light))
+switch.on()
+switch.off()
